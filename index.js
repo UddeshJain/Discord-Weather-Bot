cadbefd94415ca59abedac686d04002ee8bcfa01
UddeshJain <jainuddesh35@gmail.com>
2019-03-29 00:15:17 +0530

    make it better

--- index.js
+++ index.js
@@ -10,27 +10,31 @@ client.once('ready', () => {
 
 
 const exampleEmbed = (
-    temp,
-    maxTemp,
-    minTemp,
-    humidity,
-    wind,
-    icon,
-    author,
-    profile,
-    cityName,
-    country
+	temp,
+	maxTemp,
+	minTemp,
+	pressure,
+	humidity,
+	wind,
+	cloudness,
+	icon,
+	author,
+	profile,
+	cityName,
+	country
 ) =>
-    new Discord.RichEmbed()
-        .setColor('#0099ff')
-        .setAuthor(`Hello, ${author}`, profile)
-        .setTitle(`There is ${temp}\u00B0 C in ${cityName}, ${country}`)
-        .addField(`Maximum Temperature:`, `${maxTemp}\u00B0 C`, true)
-        .addField(`Minimum Temperature:`, `${minTemp}\u00B0 C`, true)
-        .addField(`Humidity:`, `${humidity} %`, true)
-        .addField(`Wind Speed:`, `${wind} m/s`, true)
-        .setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
-        .setFooter('Made With 💖 by @Uddesh');
+	new Discord.RichEmbed()
+		.setColor('#0099ff')
+		.setAuthor(`Hello, ${author}`, profile)
+		.setTitle(`There is ${temp}\u00B0 C in ${cityName}, ${country}`)
+		.addField(`Maximum Temperature:`, `${maxTemp}\u00B0 C`, true)
+		.addField(`Minimum Temperature:`, `${minTemp}\u00B0 C`, true)
+		.addField(`Humidity:`, `${humidity} %`, true)
+		.addField(`Wind Speed:`, `${wind} m/s`, true)
+		.addField(`Pressure:`, `${pressure} hpa`, true)
+		.addField(`Cloudiness:`, `${cloudness}`, true)
+		.setThumbnail(`http://openweathermap.org/img/w/${icon}.png`)
+		.setFooter('Made With 💖 by @Uddesh');
 
 
 const helpEmbed = () =>
@@ -80,7 +84,9 @@ client.on('message', message => {
                 let icon = apiData.data.weather[0].icon
                 let cityName = args
                 let country = apiData.data.sys.country
-                message.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, humidity, wind, icon, author, profile, cityName, country));
+                let pressure = apiData.data.main.pressure;
+                let cloudness = apiData.data.weather[0].description;
+                message.channel.send(exampleEmbed(currentTemp, maxTemp, minTemp, pressure, humidity, wind, cloudness, icon, author, profile, cityName, country));
             }).catch(err => {
                 message.reply(`Enter a vailid city name`)
             })
